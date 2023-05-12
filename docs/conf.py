73a936894328c31ac2f198fa64d1e747c7e4fb91
Stéphane Laurent <laurent_step@yahoo.fr>
2023-05-12 18:06:04 +0200

    removed specifications docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -37,8 +37,6 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinxcontrib.restbuilder'
 ]
-napoleon_google_docstring = False
-napoleon_numpy_docstring = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
